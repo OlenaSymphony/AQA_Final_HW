72312639a65851b77eec6164aff42ffa6fdfca50
Olena Gofman <olena.gofman@symphony-solutions.com>
2023-12-04 11:28:06 +0200

    added checks that fields are visible

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -15,8 +15,16 @@ export default class LoginPage {
         return cy.get('[data-test="error"]')
     }
     loginUser(username, password) {
-        this.userName().type(username).should('have.value', username)
-        this.password().type(password).should('have.value', password)
-        this.loginButton().click()
+        this.userName().type(username)
+        .should('be.visible')
+        .should('have.value', username)
+        
+        this.password().type(password)
+        .should('be.visible')
+        .should('have.value', password)
+        
+        this.loginButton()
+        .should('be.visible')
+        .click()
     }
 }
